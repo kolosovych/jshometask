31f5372c01857cd758c75f63544fe265c169603f
Halyna Kolosovych <kolosovychgalyna92@gmail.com>
2025-02-24 11:56:01 +0100

    added file

---
+++ withouta.js
@@ -0,0 +1,19 @@
+
+// //Послідовність з шести або більше символів включно з пробілами
+
+// const text = "Romeo and Juliet is a love story by William Shakespeare about two young lovers from feuding families who meet a heartbreaking fate";
+// const pattern = /[^Aa]{6,}/g;
+// const matches = text.match(pattern);
+// console.log(matches)
+
+
+
+
+
+
+// Послідовність з шести або більше символів з границями слів
+
+// const text = "Romeo and Juliet is a love story by William Shakespeare about two young lovers from feuding families who meet a heartbreaking fate";
+// const pattern = /\b[^\sAa]{6,}\b/g;
+// const matches = text.match(pattern);
+// console.log(matches)
